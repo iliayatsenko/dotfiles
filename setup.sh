f364cd836a4bac84c38b208997fa97397c2f8634
ilia <ilia@uui.ventures>
2023-09-26 21:01:38 +0300

    added installation of all utils via brew in setup.sh; added rectangle config

--- setup.sh
+++ setup.sh
@@ -1,4 +1,18 @@
 #!/bin/sh
 set -eux
 
-ln -s $(pwd)/karabiner.json ~/.config/karabiner/karabiner.json 
+brew update
+brew upgrade
+
+brew install --cask mos
+brew install --cask maccy
+brew install --cask app-cleaner
+brew install --cask karabiner-elements 
+brew install --cask monitorcontrol
+brew install --cask rectangle
+
+
+mkdir -p ~/Library/Application\ Support/Rectangle
+mkdir -p ~/.config/karabiner
+ln -sf $(pwd)/RectangleConfig.json ~/Library/Application\ Support/Rectangle/RectangleConfig.json
+ln -sf $(pwd)/karabiner.json ~/.config/karabiner/karabiner.json
