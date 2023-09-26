0f2da42b653556e9e0a043818ac42447280654b3
ilia <ilia@uui.ventures>
2023-09-26 22:31:33 +0300

    added git aliases

--- setup.sh
+++ setup.sh
@@ -1,9 +1,7 @@
 #!/bin/sh
 set -eux
 
-brew update
-brew upgrade
-
+# Install util apps
 brew install --cask mos
 brew install --cask maccy
 brew install --cask app-cleaner
@@ -12,7 +10,16 @@ brew install --cask monitorcontrol
 brew install --cask rectangle
 
 
+# Link config files
 mkdir -p ~/Library/Application\ Support/Rectangle
 mkdir -p ~/.config/karabiner
 ln -sf $(pwd)/RectangleConfig.json ~/Library/Application\ Support/Rectangle/RectangleConfig.json
 ln -sf $(pwd)/karabiner.json ~/.config/karabiner/karabiner.json
+
+
+# Git aliases
+git config --global alias.cb "rev-parse --abbrev-ref HEAD"
+git config --global alias.st "status"
+git config --global alias.nb "checkout -b"
+git config --global alias.back "checkout -"
+git config --global alias.ch "checkout"
